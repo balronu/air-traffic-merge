85ef1a27c86d3824d095a285f149b2656f184c28
balronu <81955469+balronu@users.noreply.github.com>
2026-02-23 10:05:09 +0100

    Update const.py

--- custom_components/air_traffic_merge/const.py
+++ custom_components/air_traffic_merge/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "air_traffic_merge"
 
-# Source selection (Step 1)
+# Source selection
 CONF_SOURCE_MODE = "source_mode"
 SOURCE_FR24_ONLY = "fr24_only"
 SOURCE_ADSB_ONLY = "adsb_only"
@@ -10,11 +10,10 @@ DEFAULT_SOURCE_MODE = SOURCE_BOTH
 # FR24
 CONF_FR24_ENTITY = "fr24_entity"
 
-# ADS-B source selection
+# ADS-B
 CONF_ADSB_SOURCE = "adsb_source"
-DEFAULT_ADSB_SOURCE = "url"   # "url" oder "entity"
+DEFAULT_ADSB_SOURCE = "url"  # "url" oder "entity"
 
-# ADS-B by URL or by existing entity
 CONF_ADSB_URL = "adsb_url"
 CONF_ADSB_ENTITY = "adsb_entity"
 
@@ -31,5 +30,6 @@ DEFAULT_TRACK_MODE = "callsign"
 
 CONF_TRACK_CALLSIGNS = "track_callsigns"
 CONF_TRACK_REGISTRATIONS = "track_registrations"
+
 DEFAULT_TRACK_CALLSIGNS = ""
 DEFAULT_TRACK_REGISTRATIONS = ""
